934d4a49fc94d4329c959d8ad4b4801b8816f502
sangwonlee06 <sangwonlee0622@outlook.com>
2024-10-08 15:46:43 -0700

    Initial project setup

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import Fastify from "fastify";
+
+const fastify = Fastify();
+
+fastify.get('/ping', async (request, response) => {
+    return 'pong\n';
+});
+
+const start = async () => {
+    try {
+        const PORT = 8000;
+        await fastify.listen({ port: PORT });
+        console.log(`Listening on ${PORT}`);
+    } catch (error) {
+        fastify.log.error(error);
+        process.exit(1);
+    }
+};
+
+start();
